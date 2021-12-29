7cdaddd7e26a5c24b8ce5b1f8064eea0cbdb0c5e
Noah Everett <noaheverett898@gmail.com>
2021-12-28 23:16:41 -0600

    now can read network from json file

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import os
+
 import network as code_network
 import input as code_input
 
-# Get data
+# Get/make data
 # inputs, inputs_classifications, inputs_oneHot = code_input.getData( dataSets = [ "Quest", "ON" ], dir = "/Users/noaheverett/Documents/Codes/Neural Network/Data Sets/" )
 inputs = [ [ n for n in range( 10 ) ], [ n for n in range( 10 ) ] ]
 inputs_classifications = [ "Quest" for nInput in inputs ]
@@ -12,9 +14,9 @@ del inputs[ 1: ]
 del inputs_classifications[ 1: ]
 del inputs_oneHot[ 1: ]
 
-# Make network
-network = code_network.Network( nInputs = len( inputs[ 0 ] ), nOutputs = 3, nLayers = 3, nNeurons = 4, verbose = False )
-network.save( name = "Protein_Powder_Containers_Neural_Network", dir = "/Users/noaheverett/Documents/Codes/Neural_Network/" )
+# Make/get network
+network = code_network.Network( dir = "/Users/noaheverett/Documents/Codes/Neural_Network/Protein_Powder_Containers_Neural_Network.json", nInputs = len( inputs[ 0 ] ), nOutputs = 3, nLayers = 3, nNeurons = 4, verbose = False )
+# network = code_network.Network( dir = None, nInputs = len( inputs[ 0 ] ), nOutputs = 3, nLayers = 3, nNeurons = 4, verbose = False )
 
 # nRuns = 1
 # for nRun in range( nRuns ):
@@ -31,3 +33,4 @@ print( "\n" + "Output:", output )
     # for data in inputData: 
     #     code_network.backwardPropagation( layers = layers, inputData = data, inputData_oneHot = inputData_oneHot )
 
+network.save( name = "Protein_Powder_Containers_Neural_Network", dir = "/Users/noaheverett/Documents/Codes/Neural_Network/" )
--- network.py
+++ network.py
@@ -1,9 +1,6 @@
 from numpy.random import default_rng
 from math import e
-import os
 import json
-
-import misc
 
 rng = default_rng( 0 )
 
@@ -20,45 +17,38 @@ class Network:
 
         return { "weights": weights, "biases": biases, "activation": activation }
     
-    def __saveNetwork( self, file ):
-        file.write( "Network Structure: nInputs: " + str( self.nInputs ) + "\n" )
-        file.write( "                   nOutputs: " + str( self.nOutputs ) + "\n" )
-        file.write( "                   nLayers: " + str( self.nLayers ) + "\n" )
-        file.write( "                   nNeurons: " + str( self.nNeurons ) + "\n\n" )
+    def __init__( self, dir = None, nInputs = 1, nOutputs = 1, nLayers = 1, nNeurons = 1, verbose = False ):
+        if dir == None:
+            self.structure = { "nInputs" : nInputs,
+                            "nOutputs" : nOutputs,
+                            "nLayers" : nLayers,
+                            "nNeurons" : nNeurons }
 
-
-
-        for nLayer in range( len( self.layers ) ):
-            file.write( "Layer " + misc.pad( nLayer, len( self.layers ) - 1 ) + ": Weights: " + str( self.layers[ nLayer ][ "weights" ][ 0 ][ 0 ] ) + "\n" )
-            for nNeuron in range( len( self.layers[ nLayer ][ "weights" ] ) ):
-                print( len( self.layers[ nLayer ][ "weights" ][ nNeuron ] ) )
-                for nWeight in range( len( self.layers[ nLayer ][ "weights" ][ nNeuron ] ) ):
-                    if nNeuron != 0 or nWeight != 0:
-                        file.write( " " * ( 17 + len( misc.pad( nLayer, len( self.layers ) - 1 ) ) ) + str( self.layers[ nLayer ][ "weights" ][ nNeuron ][ nWeight ] ) + "\n" )
-            file.write( " " * ( 8 + len( misc.pad( nLayer, len( self.layers ) - 1 ) ) ) + "Biases: " + str( self.layers[ nLayer ][ "biases" ][ 0 ] ) + "\n" )
-            for nNeuron in range( len( self.layers[ nLayer ][ "biases" ] ) ):
-                if nNeuron != 0:
-                    file.write( " " * ( 16 + len( misc.pad( nLayer, len( self.layers ) - 1 ) ) ) + str( self.layers[ nLayer ][ "biases" ][ nNeuron ] ) + "\n" )
-            file.write( " " * ( 8 + len( misc.pad( nLayer, len( self.layers ) - 1 ) ) ) + "Activation: " + self.layers[ nLayer ][ "activation" ] + "\n\n" )
-
-    def __init__( self, nInputs, nOutputs, nLayers, nNeurons, verbose = False ):
-        self.structure = { "nInputs" : nInputs,
-                           "nOutputs" : nOutputs,
-                           "nLayers" : nLayers,
-                           "nNeurons" : nNeurons }
-
-        # Make layers weights, biases, and activations
-        self.layers = []
-        if nLayers > 1:
-            self.layers.append( self.__makeLayer( nInputs = nInputs, nNeurons = nNeurons, activationType = "relu", verbose = verbose ) )
-            for n in range( nLayers - 2 ):
-                self.layers.append( self.__makeLayer( nInputs = nNeurons, nNeurons = nNeurons, activationType = "relu", verbose = verbose ) )
-            self.layers.append( self.__makeLayer( nInputs = nNeurons, nNeurons = nOutputs, activationType = "softmax", verbose = verbose ) ) 
+            # Make layers weights, biases, and activations
+            self.layers = []
+            if nLayers > 1:
+                self.layers.append( self.__makeLayer( nInputs = nInputs, nNeurons = nNeurons, activationType = "relu", verbose = verbose ) )
+                for n in range( nLayers - 2 ):
+                    self.layers.append( self.__makeLayer( nInputs = nNeurons, nNeurons = nNeurons, activationType = "relu", verbose = verbose ) )
+                self.layers.append( self.__makeLayer( nInputs = nNeurons, nNeurons = nOutputs, activationType = "softmax", verbose = verbose ) ) 
+            else:
+                self.layers.append( self.__makeLayer( nInputs = nInputs, nNeurons = nOutputs, activationType = "softmax", verbose = verbose ) )
         else:
-            self.layers.append( self.__makeLayer( nInputs = nInputs, nNeurons = nOutputs, activationType = "softmax", verbose = verbose ) )
+            with open( dir ) as file:
+                network = json.load( file )
+                self.structure = network[ "structure" ]
+                self.layers = network[ "layers" ]
 
-    def save( self, name, dir ):        
-        with open( dir + name + ".json", "w" ) as file:
+                print( self.structure )
+                print( self.layers )
+
+    def save( self, name, dir = None ):
+        if dir == None:
+            dir = name
+        else:
+            dir = dir + name    
+
+        with open( dir + ".json", "w" ) as file:
             json.dump( { "structure" : self.structure, "layers" : self.layers }, file, indent = 4 )
 
     def __step_forward( self, inputs, nLayer ):
@@ -98,6 +88,15 @@ class Network:
                     dWeights[ nNeuron ].append( activationDerivative * input * weight )
                 # elif self.activation == "softmax":
 
+    def __updateWeights( self, dWeights, nLayer ):
+        for nNeuron in range( len( self.weights ) ):
+            for nWeight in range( len( self.weights[ 0 ] ) ):
+                self.layers[ nLayer ][ nNeuron ][ nWeight ] += dWeights[ nNeuron ][ nWeight ]
+
+    def __updateBiases( self, dBiases, nLayer ):
+        for nBias in range( len( self.biases ) ):
+            self.layers[ nLayer ][ nBias ] += dBiases[ nBias ]
+
     def forward( self, input ):
         output = self.__step_forward( input, 0 )
         if len( self.layers ) > 1:
@@ -109,25 +108,14 @@ class Network:
     def backward( self, layers, inputData, inputData_oneHot ):
         for nLayer in range( len( layers ), 0, -1 ):
             layers[ nLayer ].backward( inputData )
-            layers[ nLayer ].updateWeights( layers[ nLayer ].dWeights )
-
-    def updateWeights( self, dWeights ):
-        for nNeuron in range( len( self.weights ) ):
-            for nWeight in range( len( self.weights[ 0 ] ) ):
-                self.weights[ nNeuron ][ nWeight ] += dWeights[ nNeuron ][ nWeight ]
-
-    def updateBiases( self, dBiases ):
-        for nBias in range( len( self.biases ) ):
-            self.biases[ nBias ] += dBiases[ nBias ]
-
-
+            layers[ nLayer ].updateWeights( layers[ nLayer ].dWeights ) 
 
 def backwardPropagation( layers, inputData, inputData_oneHot ):
     for nLayer in range( len( layers ), 0, -1 ):
         layers[ nLayer ].backward( inputData )
         layers[ nLayer ].updateWeights( layers[ nLayer ].dWeights )
 
-def getLoss( predictedOutputs, actualOutputs ):
+def __getLoss( predictedOutputs, actualOutputs ):
     loss = 0
     for predictedOutput, actualOutput in zip( predictedOutputs, actualOutputs ):
         for pOutput, aOutput in zip( predictedOutput, actualOutput ):
